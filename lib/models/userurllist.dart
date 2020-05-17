cabccfe99c6fcd3f1264c13b70e6ffe1cb3befcb
Abdulrahman Abdulkarim <olanre12waju@gmail.com>
2020-05-17 15:02:28 +0100

    First commit

---
+++ lib/models/userurllist.dart
@@ -0,0 +1,16 @@
+
+class UserUrlList {
+  final List<String> userUrls;
+
+  UserUrlList({
+    this.userUrls,
+  });
+
+  factory UserUrlList.fromJson(List<dynamic> parsedJson) {
+    List<String> uUrls = parsedJson.map((i) => i['url'].toString()).toList();
+
+    return UserUrlList(
+      userUrls: uUrls,
+    );
+  }
+}
